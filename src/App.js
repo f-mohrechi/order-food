8dc6c8ac08db60ce748d38481cdc8eec33d62d95
f-mohrechi <f.mohrechi78@gmail.com>
2024-02-23 22:55:21 +0330

    404 page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import Error from "./pages/error";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/*" element={<Error />} />
     </Routes>
   );
 }
---
+++ src/pages/error.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Error() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p className="text-6xl font-semibold">404</p>
+      <p className="text-4xl font-semibold pt-3">Oops! you caught me!</p>
+    </div>
+  );
+}
