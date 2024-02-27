791f7d9186083520dda08c842392f395e34713b8
f-mohrechi <f.mohrechi78@gmail.com>
2024-02-27 17:53:59 +0330

    modified:   src/pages/login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -45,7 +45,7 @@ function Login() {
         localStorage.setItem("user", user);
         dispatch("user", user);
         toastConfig.success("login successful");
-        navigate("/home");
+        navigate("/");
         console.log("result", data);
         console.log("user", user);
       })
