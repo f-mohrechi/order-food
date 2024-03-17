95f00a8f27f4b0683d9603676a97e6210e36540a
mahi <f.mohrechi78@gmail.com>
2024-03-17 22:31:44 +0330

    change lng of header

--- src/components/textField/TextField.jsx
+++ src/components/textField/TextField.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import Strings from "../../helper/localization/localization";
+import { useTranslation } from "react-i18next";
 
 export default function TextField() {
+  const { t } = useTranslation();
   return (
-    <div
-      className="bg-gray-100 border border-gray-200 rounded-lg flex items-center py-2.5 px-3.5 focus-within:border-primary-100 focus-within:shadow-main focus-within:shadow-main-pink
-  "
-    >
+    <div className="bg-gray-100 border border-gray-200 rounded-lg flex items-center py-2.5 px-3.5 focus-within:border-primary-100 focus-within:shadow-main focus-within:shadow-main-pink">
       <img className="" src="/img/search.svg" alt="" />
       <input
         className="bg-transparent w-full outline-none px-4"
-        placeholder={Strings.search}
+        placeholder={t("search")}
       />
     </div>
   );
